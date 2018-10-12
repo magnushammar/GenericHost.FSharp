029ac663f1c0d5b7598f8e7d9a2af9b3c887fa6d
magnushammar <magnus@tenk.se>
2018-10-12 14:41:52 +0200

    Lambda version, I just find it less readable

--- Program.fs
+++ Program.fs
@@ -15,40 +15,32 @@ open LifeTimeEventsHostedService
 [<EntryPoint>]
 let main argv =
     
-    let hostConfig (hCfg:IConfigurationBuilder) =
-        hCfg.SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("hostsettings", optional = true)
-            .AddEnvironmentVariables(prefix = "PREFIX_")
-            .AddCommandLine(argv)
-            |> ignore
-    
-    let appConfig (host: HostBuilderContext) (aCfg: IConfigurationBuilder) =
-        aCfg.SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional = true)
-            .AddJsonFile(
-            sprintf "appsettings.%s.json" (host.HostingEnvironment.EnvironmentName)
-            , optional = true)
-            .AddEnvironmentVariables(prefix =  "PREFIX_")
-            .AddCommandLine(argv)
-            |> ignore
-    
-    let serviceConfig (host: HostBuilderContext) (sCfg: IServiceCollection) =
-        sCfg.AddLogging()
-            .AddHostedService<LifetimeEventsHostedServices>()
-            .AddHostedService<TimedHostedService>()
-            |> ignore
-    
-    let loggingConfig (host: HostBuilderContext) (lCfg: ILoggingBuilder) =
-        lCfg.AddConsole()
-            .AddDebug() 
-            |> ignore
-
     let host = 
         HostBuilder()
-            .ConfigureHostConfiguration(Action<IConfigurationBuilder> hostConfig)
-            .ConfigureAppConfiguration(Action<HostBuilderContext,IConfigurationBuilder> appConfig)
-            .ConfigureServices(Action<HostBuilderContext,IServiceCollection> serviceConfig)
-            .ConfigureLogging(Action<HostBuilderContext,ILoggingBuilder> loggingConfig)
+            .ConfigureHostConfiguration(fun (hCfg:IConfigurationBuilder) ->
+                hCfg.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("hostsettings", optional = true)
+                    .AddEnvironmentVariables(prefix = "PREFIX_")
+                    .AddCommandLine(argv)
+                    |> ignore)
+            .ConfigureAppConfiguration(fun (host: HostBuilderContext) (aCfg: IConfigurationBuilder) ->
+                aCfg.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional = true)
+                    .AddJsonFile(
+                    sprintf "appsettings.%s.json" (host.HostingEnvironment.EnvironmentName)
+                    , optional = true)
+                    .AddEnvironmentVariables(prefix =  "PREFIX_")
+                    .AddCommandLine(argv)
+                    |> ignore)
+            .ConfigureServices(fun (host: HostBuilderContext) (sCfg: IServiceCollection) -> 
+                sCfg.AddLogging()
+                    .AddHostedService<LifetimeEventsHostedServices>()
+                    .AddHostedService<TimedHostedService>()
+                    |> ignore)
+            .ConfigureLogging(fun (host: HostBuilderContext) (lCfg: ILoggingBuilder) ->
+                lCfg.AddConsole()
+                    .AddDebug() 
+                    |> ignore)
             .Build()
 
     host.RunAsync() |> Async.AwaitTask |> Async.RunSynchronously
